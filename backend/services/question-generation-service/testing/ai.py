76d7f3117d562bb196e95577d4e0a517836f6ea9
Kanhu Polai <pkanhu79@gmail.com>
2025-11-13 22:40:02 +0530

    Removed AI logic to another service for future development

--- backend/services/question-generation-service/testing/ai.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-
-client = genai.Client(api_key="AIzaSyA5rwivMmKUK5g_mXn7bl3iPmNOy8j8qi8")
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Give 10 springboot questions"
-)
-print(response.text)
